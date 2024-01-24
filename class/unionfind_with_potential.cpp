3bde162f5d0c0b376505f0065c148071adbbc23b
Nichi10p <88943573+Nichi10p@users.noreply.github.com>
2024-01-25 01:02:45 +0900

    軽微な変更

--- class/unionfind_with_potential.cpp
+++ class/unionfind_with_potential.cpp
@@ -5,30 +5,29 @@
 #include <functional>
 
 namespace nichi10p {
-  // refer
-  // github.com/atcoder/ac-library/blob/master/atcoder/dsu.hpp
-  // github.com/noshi91/NoshiMochiLibrary/blob/master/Tree/UnionFindTree/PotentializedUnionFind.noshi.cpp
-  // qiita.com/drken/items/cce6fc5c579051e64fab
   template<typename potential_t>  // potential_t must have `plus`, `minus`, `0` instance (default constructed)
   class unionfind_with_potential {
     private:
-      std::vector<int>                  par;  // par[i] := (i is not root) ? (parent node) : -(component size)
-      std::vector<potential_t>          ptt;  // potential
-      std::pair<int, potential_t> const _leader(int);  // {leader, potential}
+      std::vector<int>            par;  // par[i] := (i is not root) ? (parent node) : -(component size)
+      std::vector<potential_t>    ptt;  // potential
+      std::pair<int, potential_t> _leader(int);  // {leader, potential}
     public:
       /* constructor */ explicit unionfind_with_potential(int const &);
-      int const         leader(int);
-      potential_t const potential(int);
+      int               leader(int);
+      potential_t       potential(int);
       void              merge(int, int, potential_t);
-      bool const        same(int const &, int const &);
-      potential_t const diff(int const &, int const &);
-      int const         size(int const &);
-      // std::vector<std::vector<int>> const groups();
+      bool              same(int const &, int const &);
+      potential_t       diff(int const &, int const &);
+      int               size(int const &);
+      // std::vector<std::vector<int>> groups();
   };
 }
 
 namespace nichi10p {
-  // potential_t must have `plus`, `minus`, `0` instance (default constructed)
+  // refer
+  // github.com/atcoder/ac-library/blob/master/atcoder/dsu.hpp
+  // github.com/noshi91/NoshiMochiLibrary/blob/master/Tree/UnionFindTree/PotentializedUnionFind.noshi.cpp
+  // qiita.com/drken/items/cce6fc5c579051e64fab
   template<typename potential_t>
   unionfind_with_potential<potential_t>::unionfind_with_potential(int const &N)
   : par(N, -1), ptt(N)
@@ -48,7 +47,7 @@ namespace nichi10p {
   }
 
   template<typename potential_t>
-  std::pair<int, potential_t> const unionfind_with_potential<potential_t>::_leader(int x) {
+  std::pair<int, potential_t> unionfind_with_potential<potential_t>::_leader(int x) {
     potential_t p{};
     // path halving
     while (par[x] >= 0 && par[par[x]] >= 0) {
@@ -63,12 +62,12 @@ namespace nichi10p {
   }
 
   template<typename potential_t>
-  int const unionfind_with_potential<potential_t>::leader(int x) {
+  int unionfind_with_potential<potential_t>::leader(int x) {
     return _leader(x).first;
   }
 
   template<typename potential_t>
-  potential_t const unionfind_with_potential<potential_t>::potential(int x) {
+  potential_t unionfind_with_potential<potential_t>::potential(int x) {
     return _leader(x).second;
   }
 
@@ -88,17 +87,17 @@ namespace nichi10p {
   }
 
   template<typename potential_t>
-  bool const unionfind_with_potential<potential_t>::same(int const &x, int const &y) {
+  bool unionfind_with_potential<potential_t>::same(int const &x, int const &y) {
     return leader(x) == leader(y);
   }
 
   template<typename potential_t>
-  potential_t const unionfind_with_potential<potential_t>::diff(int const &x, int const &y) {
+  potential_t unionfind_with_potential<potential_t>::diff(int const &x, int const &y) {
     return std::minus{}(potential(y), potential(x));
   }
 
   template<typename potential_t>
-  int const unionfind_with_potential<potential_t>::size(int const &x) {
+  int unionfind_with_potential<potential_t>::size(int const &x) {
     return -par[leader(x)];
   }
 }
