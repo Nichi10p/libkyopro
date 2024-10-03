38e2b8f7357329ea05c960cfee57f650e65deebf
Nichi10p <88943573+Nichi10p@users.noreply.github.com>
2024-10-04 07:08:28 +0900

    [Update] RollingHash

--- string/RollingHash.cpp
+++ string/RollingHash.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <cassert>
 #include <random>
 #include <vector>
@@ -5,91 +6,106 @@
 using namespace std;
 
 // qiita.com/keymoon/items/11fac5627672a6d6a9f6
+// www.slideshare.net/slideshow/rolling-hash-149990902/149990902
+class RHashInfo {
+  private:
+  size_t _len, _val;
+  public:
+  RHashInfo() = default;
+  RHashInfo(size_t const len, size_t const val) : _len(len), _val(val) {}
+  size_t value() const { return _val; }
+  RHashInfo& operator+=(RHashInfo const &);
+  friend RHashInfo operator+(RHashInfo h1, RHashInfo const &h2) {
+    h1 += h2;
+    return h1;
+  }
+  friend bool operator==(RHashInfo const &h1, RHashInfo const &h2) {
+    return h1._len == h2._len && h1._val == h2._val;
+  }
+};
 class RollingHash {
+  friend class RHashInfo;
   private:
   struct Mod {
     static constexpr size_t value() {
       return (static_cast<size_t>(1) << 61) - 1;
     }
+    static constexpr size_t mul(size_t const a, size_t const b) {
+      assert(a < value());
+      assert(b < value());
+      constexpr size_t mask31{(static_cast<size_t>(1) << 31) - 1};
+      constexpr size_t mask30{(static_cast<size_t>(1) << 30) - 1};
+      size_t const au{a >> 31}, ad{a & mask31};
+      size_t const bu{b >> 31}, bd{b & mask31};
+      size_t const m{ad*bu + au*bd};
+      size_t const mu{m >> 30}, md{m & mask30};
+      return au*bu*2 + mu+(md<<31) + ad*bd;
+    }
     friend constexpr size_t operator%(size_t h, Mod) {
-      h = (h >> 61) + (h & Mod::value());
-      if (h >= Mod::value())
-        h -= Mod::value();
+      h = (h >> 61) + (h & value());
+      if (h >= value())
+        h -= value();
       return h;
     }
   };
-  struct Base {
-    static size_t value() {
-      static random_device                    dev;
-      static uniform_int_distribution<size_t> randint{128, Mod::value()-1};
-      static size_t const _value{randint(dev)};
-      return _value;
-    }
-  };
-  static constexpr size_t _mul_(size_t const a, size_t const b) {
-    assert(a < Mod::value());
-    assert(b < Mod::value());
-    constexpr size_t mask31{(static_cast<size_t>(1) << 31) - 1};
-    constexpr size_t mask30{(static_cast<size_t>(1) << 30) - 1};
-    size_t const au{a >> 31}, ad{a & mask31};
-    size_t const bu{b >> 31}, bd{b & mask31};
-    size_t const m{ad*bu + au*bd};
-    size_t const mu{m >> 30}, md{m & mask30};
-    return au*bu*2 + mu+(md<<31) + ad*bd;
+  static inline size_t _base{0};
+  static inline vector<size_t> _powb;  // pow(base, i) % mod
+  static void _powb_extend(size_t const new_size) {
+    _powb.reserve(new_size);
+    while (_powb.size() < new_size)
+      _powb.push_back(Mod::mul(_powb.back(), _base) % Mod{});
   }
-  size_t _size;
-  vector<size_t> _pow /* pow(base, i) % mod */, _hash /* hash of `str[0:i)` */;
+  vector<size_t> _hash;  // hash of str[0:i)
   public:
-  static auto hash(string_view const str) {
-    size_t h{};
-    for (char const c : str)
-      h = (_mul_(h, Base::value()) + c) % Mod{};
-    return h;
+  static void set_base() {
+    assert(_base == 0);
+    random_device dev;
+    uniform_int_distribution<size_t> randint{2, Mod::value()-2};
+    _base = randint(dev);
+    _powb = {1};
+  }
+  static RHashInfo full_hash(string_view const sv) {
+    assert(_base > 1);
+    size_t h{0};
+    for (char const c : sv)
+      h = (Mod::mul(h, _base) + c) % Mod{};
+    return RHashInfo{sv.size(), h};
   }
   RollingHash() = default;
-  explicit RollingHash(string_view const str)
-  : _pow(str.size()+1, 1), _hash(str.size()+1, 0)
-  {
-    for (size_t i{0}; i < str.size(); ++i) {
-      _pow[i+1] = _mul_(_pow[i], Base::value()) % Mod{};
-      _hash[i+1] = (_mul_(_hash[i], Base::value()) + str[i]) % Mod{};
-    }
+  explicit RollingHash(string_view const str) : _hash(str.size()+1, 0) {
+    assert(_base > 1);
+    for (size_t i{0}; i < str.size(); ++i)
+      _hash[i+1] = (Mod::mul(_hash[i], _base) + str[i]) % Mod{};
+    _powb_extend(_hash.size());
   }
-  auto substr(size_t const pos, size_t const len) const {
+  RHashInfo substr(size_t const pos, size_t const len) const {
     assert(pos+len < _hash.size());
     constexpr size_t positivizer{Mod::value() * 4};
-    return (_hash[pos+len] + positivizer - _mul_(_hash[pos], _pow[len])) % Mod{};
+    return RHashInfo{len, (_hash[pos+len] + positivizer - Mod::mul(_hash[pos], _powb[len])) % Mod{}};
   }
-  auto search(string_view const str) const {
-    size_t const _h = hash(str);
+  vector<size_t> search(string_view const str) const {
+    RHashInfo h{full_hash(str)};
     vector<size_t> idx;
     for (size_t i{0}; i+str.size() < _hash.size(); ++i)
-      if (substr(i, str.size()) == _h)
+      if (substr(i, str.size()) == h)
         idx.push_back(i);
     return idx;
   }
-  auto concat(size_t const h1, size_t const h2, size_t const h2len) const {
-    return (_mul_(h1, _pow[h2len]) + h2) % Mod{};
+  RollingHash& operator+=(RollingHash const &rh) {
+    size_t const _this{_hash.back()};
+    _hash.reserve(_hash.size() + rh._hash.size());
+    for (size_t i{1}; i < rh._hash.size(); ++i)
+      _hash.push_back((Mod::mul(_this, _powb[i]) + rh._hash[i]) % Mod{});
+    _powb_extend(_hash.size());
+    return *this;
   }
-  void append(string_view const str) {
-    _pow.reserve(_pow.size() + str.size());
-    _hash.reserve(_hash.size() + str.size());
-    for (size_t i{0}; i < str.size(); ++i) {
-      _pow.push_back(
-        _mul_(_pow.back(), Base::value()) % Mod{}
-      );
-      _hash.push_back(
-        (_mul_(_hash.back(), Base::value()) + str[i]) % Mod{}
-      );
-    }
+  friend RollingHash operator+(RollingHash rh1, RollingHash const &rh2) {
+    rh1 += rh2;
+    return rh1;
   }
 };
-
-/**
- * todo
- * ハッシュ化対象を文字列以外に拡張
- * concat の再考
- *   (Hash h1, Hash h2) -> Hash のようなインターフェースにしたい
- * static 変数のスレッドセーフ初期化 (コンパイラが自動生成) を除去 @ Base::value()
- *   -fno-threadsafe-statics オプション？
- */
+RHashInfo& RHashInfo::operator+=(RHashInfo const &h) {
+  _len += h._len;
+  _val = (RollingHash::Mod::mul(_val, RollingHash::_powb[h._len]) + h._val) % RollingHash::Mod{};
+  return *this;
+}
